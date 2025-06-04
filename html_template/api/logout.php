5b4910aab81e3c97e0992f4f77653d999ceb0df4
Jonathan R. Kalinoski <139520427+Jonathan-rk@users.noreply.github.com>
2025-06-04 14:02:47 -0300

    Delete html_template directory

--- html_template/api/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../controllers/AuthController.php';
-
-$controller = new AuthController();
-$controller->logout();
-
-header('Location: /index.php');
-exit;
